92b0c57b284b354eef08731a08195e02fba77997
Ashley Lamont <benjamin.m.lamont@gmail.com>
2021-09-27 12:45:05 +1000

    Some eslint stuff

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,17 +1,20 @@
-import P5 from "p5";
+import P5 from 'p5';
 
-if(window.localStorage.getItem('instructions') === null) {
+if (window.localStorage.getItem('instructions') === null) {
   // Any needed instructions can go here.
   window.localStorage.setItem('instructions', 'done');
 }
 
-const sketch = (p: P5)=>{
+const sketch = (p: P5) => {
+  // eslint-disable-next-line no-param-reassign
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-  }
+  };
 
+  // eslint-disable-next-line no-param-reassign
   p.draw = () => {
-  }
-}
+  };
+};
 
+// eslint-disable-next-line no-new
 new P5(sketch);
